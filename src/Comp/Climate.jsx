22d6ec69bc22462441f5ca0f6675076c98ac3c24
riddhishwar <82893678+riddhishwar@users.noreply.github.com>
2023-02-17 08:12:52 +0530

    third commit

--- src/Comp/Climate.jsx
+++ src/Comp/Climate.jsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from 'react';
 import Current from '../Components/Current';
 import Forescast from '../Components/Forescast';
 import { useNavigate } from 'react-router-dom';
+import { positions } from '@mui/system';
+import Swal from 'sweetalert2';
 //calling the Weather API 
 const autocompleteURL = `https://api.weatherapi.com/v1/search.json?key=8fb866c30f1e407a89b73055231602&q=`;
 const weatherAPI = (city) => `https://api.weatherapi.com/v1/forecast.json?key=8fb866c30f1e407a89b73055231602&q=${city}&days=10&aqi=no&alerts=no`;
-const getClimate=()=>{
+const getClimate = () => {
   const data = localStorage.getItem('climate');
   if (data) {
     return JSON.parse(data);
@@ -29,6 +31,7 @@ function App() {
   useEffect(() => {
     const getDuration = setTimeout(() => {
       const fetchcityfromAPI = async () => {
+        console.log(city);
         const resp = await fetch(`https://api.weatherapi.com/v1/search.json?key=8fb866c30f1e407a89b73055231602&q=` + city);
         const data = await resp.json();
         const cityData = data.map(
@@ -72,10 +75,28 @@ function App() {
   useEffect(() => {
     localStorage.setItem('climate', JSON.stringify(climate));
   }, [climate])
+  function geolocation() {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      console.log(position);
+      const resp = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=8fb866c30f1e407a89b73055231602&q=Chennai&days=10&aqi=no&alerts=no`);
+      const data = await resp.json();
+      if (Array.isArray(data)) console.log("done");
+      else {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'error',
+          title: 'We couldn\'t recognize.Please search it',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      };
+    })
+  }
   return (
     <div className="App">
       <div className='Head'>
         <div>TradeMarke Climate Webiste</div>
+        <div><button className='buttn' onClick={() => geolocation()}>GeoLoc</button></div>
         <div><button className='buttn' onClick={() => navigate('/Storage')}>Storage</button></div>
       </div>
       <header className="App_body">
--- src/Comp/View.jsx
+++ src/Comp/View.jsx
@@ -1,13 +1,45 @@
 import React, { useEffect } from 'react'
-
+import './View.css';
+import DeleteIcon from '@mui/icons-material/Delete';
 const View = () => {
-    const data=JSON.parse(localStorage.getItem('climate'));
+    const data = JSON.parse(localStorage.getItem('climate'));
+    console.log(data);
+    const handleDelete=(blogOIndex)=>{
+        console.log(blogOIndex);
+        const _data=data.filter((doc,blogIIndex)=>{
+            if(blogIIndex!==blogOIndex){
+                return doc;
+            }
+        })
+        localStorage.setItem('climate',JSON.stringify(_data));
+        window.location.reload(false);
+    }
     return (
         <div>
             {
-                data.map((objs) => {
+                data.map((objs,objIndex) => {
                     return (
-                        <div>Hi</div>
+                        <div className='current'>
+                            <div className='head_part'>
+                                <div><b>{objs.location}</b></div>
+                                <DeleteIcon onClick={()=>handleDelete(objIndex)}></DeleteIcon>
+                            </div>
+                            <b>Current Weather</b>
+                            <div className='currentBody'>
+                                <img src={objs.current.condition.icon}></img>
+                                <span>{objs.current.condition.text}</span>
+                                <span><b>Celcius:</b>{objs.current.temp_c}<span className="sp">&#176;</span>C</span>
+                                <span><b>Frenheit:</b>{objs.current.temp_f}<span className="sp">&#176;</span>F</span>
+                                <span><b>Feels like:</b>{objs.current.feelslike_c}deg</span>
+                            </div>
+                            <div className='currentBody'>
+                                {objs.current.is_day === 1 && <span><b>Day:</b>Sunraise</span>}
+                                {objs.current.is_day === 0 && <span><b>Day:</b>Sunset</span>}
+                                <span><b>Wind Speed:</b>{objs.current.wind_kph}deg</span>
+                                <span><b>Humidity:</b>{objs.current.humidity}</span>
+                                <span><b>Pressure:</b>{objs.current.pressure_in}</span>
+                            </div>
+                        </div>
                     )
                 })
             }
